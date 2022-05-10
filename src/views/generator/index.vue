52ec678abe64d94ff79bd58e996b9c6b3a11426e
wangjiahao <1332661444@qq.com>
2022-05-10 14:50:40 +0800

    隐藏多余字段信息

--- src/views/generator/index.vue
+++ src/views/generator/index.vue
@@ -24,10 +24,10 @@
     <el-table ref="table" v-loading="crud.loading" :data="crud.data" style="width: 100%;" @selection-change="crud.selectionChangeHandler">
       <el-table-column type="selection" width="55" />
       <el-table-column :show-overflow-tooltip="true" prop="tableName" label="表名" />
-      <el-table-column :show-overflow-tooltip="true" prop="engine" label="数据库引擎" />
-      <el-table-column :show-overflow-tooltip="true" prop="coding" label="字符编码集" />
+      <!--      <el-table-column :show-overflow-tooltip="true" prop="engine" label="数据库引擎" />-->
+      <!--      <el-table-column :show-overflow-tooltip="true" prop="coding" label="字符编码集" />-->
       <el-table-column :show-overflow-tooltip="true" prop="remark" label="备注" />
-      <el-table-column prop="createTime" label="创建日期" />
+      <!--      <el-table-column prop="createTime" label="创建日期" />-->
       <el-table-column label="操作" width="160px" align="center" fixed="right">
         <template slot-scope="scope">
           <el-button size="mini" style="margin-right: 2px" type="text">
